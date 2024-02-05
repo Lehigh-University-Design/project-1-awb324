b68b28a4f755fac0a7c055158694e1d9e25e40fb
awb324 <awb324@lehigh.edu>
2024-02-05 10:16:21 -0500

    n

--- js/navigator.js
+++ js/navigator.js
@@ -2,11 +2,20 @@
  *  Websites list in order
  */
 var websites = [
-    "1.html",
-    "2.html",
-    "3.html",
-    "4.html"
-];
+    "https://lehigh-university-design.github.io/project-1-awb324/",
+    "https://lehigh-university-design.github.io/project-1-sit225/",
+    "https://lehigh-university-design.github.io/project-1-lar326/",
+    "https://lehigh-university-design.github.io/project-1-adrawoot/",
+    "https://lehigh-university-design.github.io/project-1-mmurata22/",
+    "https://lehigh-university-design.github.io/project-1-SSShee77y/",
+    "https://lehigh-university-design.github.io/project-1-eyt225/”",
+    "https://lehigh-university-design.github.io/project-1-shirls326/”",
+    "https://lehigh-university-design.github.io/project-1-Colin941/",
+    "https://lehigh-university-design.github.io/project-1-ellafall/",
+    "https://lehigh-university-design.github.io/project-1-laurj46/",
+    "https://lehigh-university-design.github.io/project-1-kaydolan/",
+    "https://lehigh-university-design.github.io/project-1-dsw225/",
+    ];
 
 /*
  *  Website index variable
@@ -14,13 +23,8 @@ var websites = [
 var currentIndex = 0;
 
 window.onload = function getCurrentIndex() {
-    var path = window.location.href; 
-    var page = path.split("/");
-
-    if (page.includes("https") || page.includes("http"))
-        currentIndex = websites.indexOf(path);
-    else
-        currentIndex = websites.indexOf(page.pop());
+    var path = window.location.href.split("#")[0];
+    currentIndex=websites.indexOf(path);
 }
 
 /*
